5d355f47fb0495b9b49a821bda274f5a1495b562
dusanCvetnic <dusancvetnic@gmail.com>
2021-06-11 18:27:39 +0200

    Update http messages

--- 03-back-end/src/controllers/user.controller.ts
+++ 03-back-end/src/controllers/user.controller.ts
@@ -1,12 +1,17 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from 'express';
 import { connect } from "../common/database";
-import { UserModel } from "../models/user.model";
+import { UserModel } from '../models/user.model';
 import * as bcrypt from "bcrypt";
 
 export async function getAllUsers(req: Request, res: Response): Promise<Response> {
     try {
         const conn = await connect()
         const result = await conn.query('SELECT * FROM user;')
+
+        if(!result[0][0]?.email){
+            return res.status(404).send('Ne postoje korisnici')
+        }
+
         return res.json(result[0])
     } catch (error) {
         res.sendStatus(500)
@@ -14,12 +19,36 @@ export async function getAllUsers(req: Request, res: Response): Promise<Response
     }
 }
 
-export async function getUserById(req: Request, res: Response): Promise<Response> {
+async function getUserByEmail(email): Promise<UserModel> {
+    try {
+        const conn = await connect()
+        const result = await conn.query('SELECT * FROM user WHERE email = ?;', [email])
+
+        return result[0][0] as UserModel
+    } catch (error) {
+        console.log(error?.sqlMessage)
+    }
+}
+
+export async function getUserById(req: Request, res: Response, next: NextFunction): Promise<Response> {
     const id = req.params.id
     try {
         const conn = await connect()
         const result = await conn.query('SELECT * FROM user WHERE user_id = ?;', [id])
-        return res.json(result[0])
+        
+        if(!result[0][0]?.email){
+            return res.status(404).send('Trazeni korisnik ne postoji')
+        }
+
+        const user: UserModel = {
+            forename: result[0][0]?.forename,
+            surname: result[0][0]?.surname,
+            email: result[0][0]?.email,
+            password: result[0][0]?.password_hash,
+            role: result[0][0]?.role,
+        }
+        
+        return res.json(user)
     } catch (error) {
         res.sendStatus(404)
         console.log(error?.sqlMessage)
@@ -40,8 +69,11 @@ export async function createUser(req: Request, res: Response) {
                 email = ?,
                 password_hash = ?,
                 role = ?;`, [newUser.forename, newUser.surname, newUser.email, passwordHash, newUser.role])
-        res.sendStatus(200)
+
+        const user: UserModel = await getUserByEmail(newUser.email)
+        console.log(user)
         console.log(`Dodat je korisnik ${newUser.forename} ${newUser.surname} sa ulogom ${newUser.role}`)
+        res.sendStatus(200)
     } catch (error) {
         console.log(error?.sqlMessage)
     }
@@ -94,4 +126,29 @@ export async function deleteUserById(req: Request, res: Response) {
     } catch (error) {
         console.log(error?.sqlMessage)
     }
+}
+
+export async function loginUser(req: Request, res: Response) {
+    const email = req.body.email
+    const passwordToCheck = req.body.password
+    if(email && passwordToCheck){
+        try {
+            const conn = await connect()
+            const result = await conn.query(`
+                SELECT
+                    *
+                FROM 
+                    user 
+                WHERE
+                    email = ?;`, [email])
+            
+            //userPassword = result[0][0].password_hash
+            if(bcrypt.compareSync(passwordToCheck, result[0][0].password_hash)){
+                console.log(`ISTI`)
+            }
+    
+        } catch (error) {
+            console.log(error?.sqlMessage)
+        }
+    }    
 }
--- 03-back-end/src/routes/user.routes.ts
+++ 03-back-end/src/routes/user.routes.ts
@@ -1,17 +1,19 @@
 import { Router } from "express";
 import { userSchema } from "../schemas/user.schema"
 import { validateRequest } from '../middlewares/validateRequest';
-import { createUser, deleteUserById, getAllUsers, getUserById, updateUserById } from "../controllers/user.controller";
+import { createUser, deleteUserById, getAllUsers, getUserById, loginUser, updateUserById } from "../controllers/user.controller";
 
 const router = Router()
 
 router.route('/')
-      .get(getAllUsers)
-      .post(userSchema, validateRequest, createUser)
+    .get(getAllUsers)
+    .post(userSchema, validateRequest, createUser)
 
 router.route('/:id')
-      .get(getUserById)
-      .put(userSchema, validateRequest, updateUserById)
-      .delete(deleteUserById)
+    .get(getUserById)
+    .put(userSchema, validateRequest, updateUserById)
+    .delete(deleteUserById)
+router.route('/login')
+    .post(loginUser)
 
 export default router;  
