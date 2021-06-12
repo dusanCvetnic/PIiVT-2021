20d285fa34a7c6f561e5e27361222637ff8b6b48
dusanCvetnic <dusancvetnic@gmail.com>
2021-06-12 14:14:49 +0200

    Correcting errors

--- 03-back-end/src/schemas/review.chema.ts
+++ 03-back-end/src/schemas/review.chema.ts
@@ -3,14 +3,13 @@ import { body } from 'express-validator';
 const reviewSchema = [
     body('rating')
         .exists({checkFalsy: true})
-        .isNumeric()
-        .matches(/^0|1|2|3|4|5$/)
-        .withMessage('Ocena mora biti broj od 0 do 5'),
+        .isInt({min: 0, max: 5})
+        .withMessage('Ocena mora biti ceo broj od 0 do 5'),
     body('ratedUserId')    
         .exists({checkFalsy: true})
         .isNumeric()
         .withMessage('Review mora sadrzati informacije ko je ocenjen'),
-    body('ruserWhoRatedId')    
+    body('userWhoRatedId')    
         .exists({checkFalsy: true})
         .isNumeric()
         .withMessage('Review mora sadrzati informacije ko je ocenio')
